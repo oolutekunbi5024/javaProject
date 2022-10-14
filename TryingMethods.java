eee4662c1085bf0c11d2a7adc82d90fa5997e30f
Olamide <olamide.olutekunbi@meltwater.org>
2022-10-14 20:02:58 +0000

    Dynamic Class Creation

---
+++ TryingMethods.java
@@ -0,0 +1,46 @@
+
+/**
+ * TryingMethods Class
+ */
+public class TryingMethods {
+
+    public static void main(String[] args) {
+     
+    }
+
+    public static String dynamicMethod1(int daysOfTheWeek){
+    String days;
+        switch (daysOfTheWeek) {
+        case 1:
+        days="Monday";
+        break;
+    case 2:
+        days="Tuesday";
+        break;
+    case 3:
+        days="Wednesday";
+        break;
+        case 4:
+        days="Thursday";
+        break;
+    case 5:
+        days="Friday";
+        break;
+    case 6:
+        days="Saturday";
+        break;
+    case 7:
+        days="Sunday";
+        break;
+    
+        default:
+        days="This is day is not a day of the Week";
+            break;
+    }
+    return days;
+}
+
+
+
+}
+
