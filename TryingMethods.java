17a292d559087ca34d2a31807cfc100ee968730f
Olamide <olamide.olutekunbi@meltwater.org>
2022-10-14 21:10:06 +0000

    Adding second static method

---
+++ TryingMethods.java
@@ -0,0 +1,23 @@
+/**
+ * TryingMethods
+ */
+public class TryingMethods {
+
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i = 12; i < 103; i++) {
+            if (i%2==0) {
+                sum+=i;
+                            }
+           
+        }   
+        System.out.print(sum); 
+    }
+    public static void forLoop(){
+        int sum=0;
+        for (int i = 12; i < 103; i++) {
+        sum=+i;
+
+        }
+    }
+}
