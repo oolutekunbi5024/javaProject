8ba03acfe9afe5dd16b05e105b1b96851b655c3a
Olamide <olamide.olutekunbi@meltwater.org>
2022-10-14 20:43:32 +0000

    first static method

---
+++ TryingMethods.java
@@ -0,0 +1,20 @@
+/**
+ * TryingMethods
+ */
+public class TryingMethods {
+
+    public static void main(String[] args) {
+       
+    }
+//first static method
+    static int sum() {
+    int i=1;
+        int sumValue=0;
+        while (i<1000) {
+            sumValue+= i;
+            i++;
+          
+}
+    return sumValue;
+    }
+}
