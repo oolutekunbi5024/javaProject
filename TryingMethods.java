b7859983579d64858c7b9a80d904c99d2a84101c
Olamide <olamide.olutekunbi@meltwater.org>
2022-10-14 20:55:21 +0000

    dynamic method creation

---
+++ TryingMethods.java
@@ -0,0 +1,18 @@
+/**
+ * TryingMethods
+ */
+public class TryingMethods {
+
+void dynamic2(){
+        int score=70;
+    if (score>=70) {
+    System.out.println("Excellent");
+    if (score>=60) {
+    System.out.println("Good");
+}
+    else if (score>=50) {
+    System.out.println("Pass");
+}
+}
+    }
+}
